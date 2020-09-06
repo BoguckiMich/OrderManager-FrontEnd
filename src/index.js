862420556127d784960c4b02fd5c2150c23c0322
BoguckiMich <bogucki.mich@gmail.com>
2020-09-06 11:27:03 +0200

    refactored order list, details of users

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-     />
+     <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
