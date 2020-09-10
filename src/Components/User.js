7471d561e17c98768a9da61183df1bbfe666a996
BoguckiMich <bogucki.mich@gmail.com>
2020-09-10 10:11:06 +0200

    redux

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,39 +1,64 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import UserDetails from "./UserDetails"
+import UserDetails from "./UserDetails";
 import Paper from "@material-ui/core/Paper";
-import "./User.css"
-import {Button} from "@material-ui/core";
+import "./User.css";
+import { Button } from "@material-ui/core";
+import * as actions from "./store/actions";
+import { connect } from "react-redux";
 
-const User = ({fetchURL}) => {
+const User = ({ users, onUsersFetch }) => {
+  const [userId, setUserId] = useState("");
+  const [userDetails, setUserDetails] = useState(false);
 
-    const [users, setUsers] = useState([]);
-    const [userId, setUserId] = useState('')
-    const [userDetails, setUserDetails] = useState(false)
+  const fetchData = async () => {
+    const request = await axios.get("http://localhost:8080/user", {
+      auth: { username: "michalek", password: "qwe123" },
+      method: "GET",
+    });
+    console.log(request.data);
+    onUsersFetch(request.data);
+    // console.log(orders)
+  };
 
-    const fetchData = async () => {
-        const request = await axios.get("http://localhost:8080/user", {auth: {username: "michalek", password: "qwe123"}, method: "GET"});
-        console.log(request.data)
-        setUsers(request.data)
-        // console.log(orders)
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(()=>{
-        fetchData();
-    },[])
-
-    return (
-        <div className="userContainer">
-            {users.map((user)=><Paper className="user" key={user.id}>
-                <p hidden>{user.id}</p>
-                <h1>{user.firstName}</h1>
-                <Button size="small" color="secondary" onClick={()=>{setUserDetails(!userDetails); setUserId(user.id)}}>
-                    Details
-                </Button>
-                {userDetails ? <UserDetails user={user} /> : null}
-            </Paper>)}
-        </div>
-    );
+  return (
+    <div className="userContainer">
+      {users.map((user) => (
+        <Paper className="user" key={user.id}>
+          <p hidden>{user.id}</p>
+          <h1>{user.firstName}</h1>
+          <Button
+            size="small"
+            color="secondary"
+            onClick={() => {
+              setUserDetails(!userDetails);
+              setUserId(user.id);
+            }}
+          >
+            Details
+          </Button>
+          {userDetails ? <UserDetails user={user} /> : null}
+        </Paper>
+      ))}
+    </div>
+  );
 };
 
-export default User;
+const mapStateToProps = (state) => {
+  return {
+    users: state.reducer.users,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onUsersFetch: (users) =>
+      dispatch({ type: actions.USERS_FETCHED, users: users }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import userReducer from "./Components/store/reducers";
+import { Provider } from "react-redux";
+
+const rootReducer = combineReducers({
+  reducer: userReducer,
+});
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
